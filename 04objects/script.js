5aaa0ef2233009cc067aca254fa7c74c733a3dae
Pierre Bretéché <pbreteche@dawan.fr>
2017-12-05 11:21:02 +0100

    réécriture avec classes ES2015

--- 04objects/script.js
+++ 04objects/script.js
@@ -1,45 +1,36 @@
 'use strict';
 
-var Picture = function(src) {
-    this.src = src;
+class Picture {
+    constructor(src) {
+        this.src = src;
+    }
+    
+    toHTML() {
+        return '<img src="' + this.src + '" />';
+    }    
 }
 
-Picture.prototype.toHTML = function() {
-    return '<img src="' + this.src + '" />';
-}
-
-var GalleryComponent = function(selector) {
-    this.baseElement = document.querySelector(selector);
-    this.pictures = [];
-}
-
-GalleryComponent.prototype.add = function(picture) {
-    if (!picture instanceof Picture) {
-        throw new Error('La gallerie n\'accepte que des photos');
+class GalleryComponent {
+    constructor(selector) {
+        this.baseElement = document.querySelector(selector);
+        this.pictures = [];
     }
 
-    this.pictures.push(picture);
-}
-
-GalleryComponent.prototype.redraw = function(){
-    let htmlOutput = '';
-/*
-    for (let i=0; i < this.pictures.length; i++ ) {
-        
+    add(picture) {
+        if (!picture instanceof Picture) {
+            throw new Error('La gallerie n\'accepte que des photos');
+        }
+    
+        this.pictures.push(picture);
     }
-    this.pictures.forEach(function(elem, index) {
-        var localVar = '...'; // portée d'une variable = la fonction
-        //htmlOutput += elem.toHTML(); // variable output accédée depuis le «scope» parent
-    });
-*/
-    // variable de bloc déclarée avec «let»
-    // marche aussi avec «const» (ne peut être réaffectée)
-    for (const picture of this.pictures) {
-        htmlOutput += picture.toHTML(); 
+    redraw(){
+        let htmlOutput = '';
+        for (const picture of this.pictures) {
+            htmlOutput += picture.toHTML(); 
+        }
+    
+        this.baseElement.innerHTML = htmlOutput;
     }
-    console.debug('redraw', this);
-
-    this.baseElement.innerHTML = htmlOutput;
 }
 
 document.addEventListener('DOMContentLoaded', function(){
