b59ead26fd277772ba97b31fa219fcefc2e41932
Pierre Bretéché <pbreteche@dawan.fr>
2017-12-06 11:20:34 +0100

    modules

---
+++ 10modules/Example.js
@@ -0,0 +1,10 @@
+
+export class ExampleClass {
+    constructor(){
+        console.log('ça marche !')
+    }
+}
+
+export const PI = 3.14;
+
+const GN = 1.61;
---
+++ 10modules/main.js
@@ -0,0 +1,9 @@
+'use strict';
+
+//import {ExampleClass, PI} from './Example.js';
+
+import * as ex from './Example.js';
+
+const instance = new ex.ExampleClass();
+
+console.log(ex.PI)
